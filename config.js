a1f2b1705b196813bad5cdd5d5a66b0b80c3fe2f
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-04-30 14:47:20 +0300

    Added port and ip config in start options

--- config.js
+++ config.js
@@ -2,5 +2,7 @@ module.exports = {
     "db_type": "mongodb",
     "db_host": "mongodb://10.240.212.94:27017/zenx",
     "db_user": "",
-    "db_pass": ""
+    "db_pass": "",
+    "zenx_client_bind": "10.240.212.94",
+    "zenx_client_port": "10000"
 };
