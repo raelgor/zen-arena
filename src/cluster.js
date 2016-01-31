fd14226378d204b5d58334f767cbf348df70fc15
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-01-31 20:04:41 +0200

    Removed default config.js

--- src/cluster.js
+++ src/cluster.js
@@ -6,19 +6,21 @@ process.title = 'zen-arena-cs';
 const zenx = require('zenx');
 const os = require('os');
 const path = require('path');
-const interfaces = os.networkInterfaces();
-const defaultInterface = 'eth0' in interfaces ? interfaces.eth0[0].address : 'localhost';
+const config = require('../config');
+const defaultInterface = config.bind_ip;
+const port = config.port;
 
 // Start server
 global.app = new zenx.Server({
 	bind: defaultInterface,
-	port: 10000,
+	port,
 	ws: true,
 	static: path.resolve(__dirname + '/../assets')
 });
 
 app.config = {
-    defaultInterface
+    defaultInterface,
+    port
 };
 
 // Connect to cache servers
