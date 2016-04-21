be4fb213cb3a66ad3621089d1f27b683f22b7e71
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-04-21 21:10:35 +0300

    !

--- src/cluster.js
+++ src/cluster.js
@@ -83,9 +83,9 @@ process.on('message', message => co(function*(){
 
     global.config = message;
     //global.cacheClient = new cache.Client(config.cache_server);
-
+try{
     dataTransporter.setMongosClient(make_mongo_url(config.systemDatabase));
-
+}catch(err){throw err;}
     log('Done. Waiting for connect.');
 
     // Swallow errors
