324446ac860a941e1699bd21986ae5516719a910
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-05-05 20:30:25 +0300

    Added ui.Darkness client class

---
+++ client/ui/ui.Darkness.js
@@ -0,0 +1,33 @@
+za.ui.Darkness = function(){
+
+   var element = this.element = $('<div>');
+   var object = this;
+
+   element.addClass('darkness-container');
+
+   this.spawn = function() {
+      if($('.darkness-container').length)
+         $('.darkness-container').remove();
+      $('body').append(element);
+
+      object.emit('spawnstart');
+      element.animate({opacity:1}, 400, 'swing', function(){
+         object.emit('spawnend');
+      });
+   };
+
+   this.fade = function() {
+      object.emit('fadestart');
+      element.animate({opacity:0}, 300, 'swing', function(){
+         element.remove();
+         object.emit('fadeend');
+      });
+   };
+
+   element.click(function(e){
+      object.emit('click', e);
+   });
+
+};
+
+za.ui.Darkness.prototype = EventEmitter2.prototype;
