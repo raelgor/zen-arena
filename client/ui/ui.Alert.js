dff51f74e1b7d5498244549e5066d92d7e17db3c
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-05-06 05:21:06 +0300

    Completed alert

--- client/ui/ui.Alert.js
+++ client/ui/ui.Alert.js
@@ -1,10 +1,25 @@
+/*
+
+Options:
+   title: Window title core text id
+   message: Window message core text id
+   cancelable: Boolean about whether the window can be disposed or not
+
+*/
 za.ui.Alert = function(options){
 
-   var cancelable = options.cancelable;
+   options = typeof options === 'object' ? options : {};
+   var cancelable = 'cancelable' in options ? options.cancelable : true;
+
+   var title = 'title' in options ? options.title : '';
+   var message = 'message' in options ? options.message : '';
+
+   title = clientData.core_text[title];
+   message = clientData.core_text[message];
 
    var object = this;
    var darkness = new za.ui.Darkness();
-   //var dialogButtons = new za.ui.DialogButtons([{ ok: 'ok' }]);
+   var dialogButtons = new za.ui.DialogButtons([{ id: 'ok', text: 'ok' }]);
    var centeredWindow = new za.ui.CenteredWindow({
       fields: 'title description buttons',
       cancelable: true
@@ -13,29 +28,33 @@ za.ui.Alert = function(options){
    centeredWindow.updateField('title', clientData.core_text[options.title]);
    centeredWindow.updateField('description', clientData.core_text[options.message]);
 
-   //centeredWindow.updateField('buttons', dialogButtons.element);
+   centeredWindow.updateField('buttons', dialogButtons.element);
 
    this.spawn = function(){
       darkness.spawn();
       centeredWindow.spawn();
+      dialogButtons.focus('ok');
+      return object;
    };
 
    this.fade = function(){
       darkness.hide();
       centeredWindow.dispose();
+      return object;
    };
 
    this.dispose = function(){
       object.emit('disposed');
       darkness.fade();
       centeredWindow.dispose();
+      return object;
    };
 
    if(cancelable)
       darkness.on('click', this.dispose);
 
    centeredWindow.on('disposed', this.dispose);
-   //dialogButtons.on('submit', this.dispose);
+   dialogButtons.on('click', this.dispose);
 
    this.darkness = darkness;
    this.window = centeredWindow;
