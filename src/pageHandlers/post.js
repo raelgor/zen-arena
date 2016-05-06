3ce2faede13a3e1eb9ac6f9fe3ce22eec929168c
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-05-06 19:56:15 +0300

    Introduced getPostViewData and post meta data

--- src/pageHandlers/post.js
+++ src/pageHandlers/post.js
@@ -9,6 +9,19 @@ module.exports = r;
 
 r.setHandler(response => co(function*() {
 
+   var post = yield dataTransporter.getPostViewData(+response.request.params.post_id);
+
+   response.pageData.meta.title =
+      post.title ||
+      (response.pageData.coreText.post_by + ' ' +
+       post.publisher_display_name + ' - ' +
+       appConfig.site_name);
+
+   response.pageData.meta.description = post.text.substr(0, 200) + ' ...';
+
+   if(post.image)
+      response.pageData.meta.og_image = post.image;
+
    response.responseData = yield factory.index.make(
        response.request,
        response.pageData,
