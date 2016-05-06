b3d3742a5dec206e8e6d025c84ee9777346c4495
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-05-06 18:22:59 +0300

    Completed default and home metadata

--- src/fn/make_default_meta_data.js
+++ src/fn/make_default_meta_data.js
@@ -4,7 +4,11 @@ module.exports = core_text => {
 
    return {
 
-      title: core_text.title
+      title: core_text.title,
+      og_image: '/img/og.jpg',
+      og_description: core_text.og_description,
+      og_site_name: appConfig.site_name,
+      og_locale: appConfig.og_locale
 
    };
 
