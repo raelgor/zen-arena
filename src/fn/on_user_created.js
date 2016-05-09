d3222e7ecaad38337e9ce95c6a922f794a32c0a2
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-05-09 05:05:49 +0300

    Namespace collection update on user creation

--- src/fn/on_user_created.js
+++ src/fn/on_user_created.js
@@ -3,6 +3,12 @@
 
 module.exports = user => co(function*(){
 
+   yield mongos.collection('namespaces').insert({
+      id: +user.get('id'),
+      collection: 'users',
+      namespace: null
+   });
+
    if(!user.get('unsubscribe_all_token')) {
       user.set('unsubscribe_all_token', uuid());
       yield user.updateRecord();
