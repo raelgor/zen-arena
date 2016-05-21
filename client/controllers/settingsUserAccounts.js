d079db94ce47021c8041b855d1e10e8f0a48185c
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-05-21 20:16:20 +0300

    t

--- client/controllers/settingsUserAccounts.js
+++ client/controllers/settingsUserAccounts.js
@@ -8,7 +8,12 @@ function bnetCodeListener(msg) {
 za.controllers.settingsUserAccounts = new za.Controller(function(element){
 
    $(element).find('button.bnet').click(function(){
-      window.bnet = window.open('/api/battlenet/auth', 'Battle.net Authentication', 'width=400,height=400');
+      // if(/chrome|crios/i.test(navigator.userAgent) && za._touch) {
+
+         window.location.href = '/api/battlenet/auth';
+
+      // } else
+         // window.bnet = window.open('/api/battlenet/auth', 'Battle.net Authentication', 'width=400,height=400');
    });
 
 });
--- src/api/battlenet/regcb.js
+++ src/api/battlenet/regcb.js
@@ -13,13 +13,10 @@ passport.use(new BnetStrategy({
     clientSecret: appConfig.battle_net.secret,
     callbackURL: appConfig.battle_net.callbackUrl
 }, function(accessToken, refreshToken, profile, done) {
-   console.log('passport itself:');
-   console.log({accessToken, profile});
     return done(null, profile);
 }));
 
 r.setHandler((response, req, res) => co(function*(){
-   console.log('regcb:');
-   console.log(yield job.getBattlenetProfile(req.query.code));
-   res.end(`<script>window.opener.postMessage('bnc:${req.query.code}', '*');window.close();</script>`);
+   yield job.getBattlenetProfile(req.query.code);
+   res.end(`<script>window.opener.postMessage('bnc:${req.query.code}', '*');window.close();window.location.href='/settings/accounts';</script>`);
 }).catch(error => instance.emit('error', error)));
