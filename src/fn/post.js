095e87ddb8746cb4007d57bf463d2b1d7e01f2cd
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2016-05-21 19:40:03 +0300

    fix

--- src/fn/post.js
+++ src/fn/post.js
@@ -7,16 +7,26 @@
  * @returns {Promise}
  */
 module.exports = (url, data, headers) => {
+
    let lib = /^https/.test(url) ? https : http;
-   let port = /^https/.test(url) ? 443 : 80;
+
+   let port =  url.match(/^http[s]{0,1}:\/\/[^\/\:]*\:([0-9]{1,5})/);
+
+   port = port && port[1] ? +port[1] : /^https/.test(url) ? 443 : 80;
+
+   let path = url.match(/^http[s]{0,1}:\/\/[^\/]*(\/.*)$/);
+
+   path = path && path[1] ? path[1] : undefined;
+
    let r;
    let promise = new Promise(resolve => r = resolve);
    var rdata = querystring.stringify(data);
+
    var request = lib.request({
       method: 'post',
       hostname: url.match(/^http[s]{0,1}:\/\/([^\/\:]*)/)[1],
-      port: +url.match(/^http[s]{0,1}:\/\/[^\/\:]*\:([0-9]{1,5})/)[1] || port,
-      path: url.match(/^http[s]{0,1}:\/\/[^\/]*(\/[.]*)$/)[1] || '/',
+      port,
+      path,
       headers
    }, response => {
       let data = '';
@@ -25,8 +35,11 @@ module.exports = (url, data, headers) => {
       response.on('end', () => r(data));
       response.on('error', r);
    });
+
    request.on('error', r);
    request.write(rdata);
    request.end();
+
    return promise;
+
 };
